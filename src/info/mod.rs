29d961486332c31e4b71536a0c90221822a64e00
PeroSar <perosar1111@gmail.com>
2021-09-12 01:13:03 +0530

    src/info: Remove unnecessary variable
    There's no need of 2 separate variables: one with format macro and another to make the request. It is better in a oneliner.
    
    Signed-off-by: PeroSar <perosar1111@gmail.com>

--- src/info/mod.rs
+++ src/info/mod.rs
@@ -2,10 +2,9 @@ mod structs;
 use structs::ApiResp;
 
 #[tokio::main]
-pub async fn full<T: std::fmt::Display>(user: T) ->  ApiResp {
+pub async fn full<T: std::fmt::Display>(user: T) -> ApiResp {
     let client = reqwest::Client::new();
-    let fmt = format!("https://api.intellivoid.net/spamprotection/v1/lookup?query={}", user);
-    let res = client.get(fmt)
+    let res = client.get(format!("https://api.intellivoid.net/spamprotection/v1/lookup?query={}", user))
         .header("User-Agent", "SpamProtectionBot-rs")
         .send()
         .await
