e3321bbeb2549456d9bf118df7c8095bdec6678f
Cyber Knight <cyberknight755@gmail.com>
2021-09-12 07:21:33 +0800

    examples: Restructured the example
    This was done in correspondence to f6a1d44.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- examples/cli-arg.rs
+++ examples/cli-arg.rs
@@ -1,21 +1,83 @@
 use spamprotection::info;
-use std::env;
+use std::{env, process};
 
-/* We would need to first to declare the spamprotection library with use declaration.
-   We would also need to declare the env trait from std library as args variable will be collecting the arguments passed to the program.
-*/
 
 fn main()
 {
-    /* Here we will need to assign a Vector type to args variable to collect the arguments passed to program.
-       Then we will need to assign String type to arg variable to parse the arguments passed.
-    */
     let args: Vec<String> = env::args().collect();
     let arg: String = args[1].parse().expect("invalid account");
+    
+    if info::get_success(&arg) {
+	println!("You have successfully connected to Intellivoid database!\n");
+    } else {
+	println!("This {} is not in Intellivoid database or there may be a problem with the API at this moment.", info::get_type(&arg));
+	process::exit(1);
+    }
 
-    /* Here's where we will start by declaring the info variable and assign it to the info module in spamprotection library
-       which calls the full() method with given the arg variable as the value.
-    */
-    let info = info::full(arg);
+    if info::get_resp(&arg) == 200 {
+	println!("It was a successful response!\n");
+    }
+	
+	if info::get_bl(&arg) {
+	    println!("This {} is blacklisted due to {} with the {} flag.\n", info::get_type(&arg), info::get_reason(&arg).unwrap_or("None".to_string()), info::get_flag(&arg).unwrap_or("None".to_string()));
+	} else {
+	    println!("This {} is not blacklisted!\n", info::get_type(&arg));
+	}	
+    if info::get_flag_evade(&arg) {
+	println!("Beware! This {} has an evading flag. This is the original PTID of the person: {}", info::get_type(&arg), info::get_original_ptid(&arg).unwrap_or("None".to_string()));
+    }
+    
+    if info::get_potential(&arg) {
+	println!("Beware! This {} is a potential spammer!!", info::get_type(&arg));
+    }
+
+    if info::get_user_verified(&arg) {
+	println!("This {} is verified by Intellivoid!", info::get_type(&arg));
+    }
+
+    if info::get_flag_raid(&arg) {
+	println!("Beware! This {} has a raiding flag.", info::get_type(&arg));
+    }
+
+    if info::get_flag_spam(&arg) {
+	println!("Beware! This {} has a spammer flag.", info::get_type(&arg));
+    }
+
+    if info::get_flag_scam(&arg) {
+	println!("Beware! This {} has a scammer flag.", info::get_type(&arg));
+    }
+
+    if info::get_flag_private(&arg) {
+	println!("Beware! This {} has a private flag.", info::get_type(&arg));
+    }
+
+    if info::get_flag_sp(&arg) {
+	println!("Beware! This {} has a special flag.", info::get_type(&arg));
+    }
+
+    if info::get_flag_namespam(&arg) {
+	println!("Beware! This {} has a namespam flag.", info::get_type(&arg));
+    }
+
+    if info::get_flag_imper(&arg) {
+	println!("Beware! This {} has a impersonation flag.", info::get_type(&arg));
+    }
+
+    if info::get_flag_massadd(&arg) {
+	println!("Beware! This {} has a mass adding flag.", info::get_type(&arg));
+    }
+
+    if info::get_flag_piracy(&arg) {
+	println!("Beware! This {} has a piracy flag.", info::get_type(&arg));
+    }
+
+    if info::get_flag_cacp(&arg) {
+	println!("Beware! This {} has a child abuse flag.", info::get_type(&arg));
+    }
+
+    // You can also alternatively use the full() method in info module
+    // This is however tedious and not suggested.
+    let info = info::full(&arg);
+
     println!("{:#?}", info);
 }
