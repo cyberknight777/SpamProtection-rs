f6a1d4488dceabd4ae138630946fb9782c1d0ad4
Cyber Knight <cyberknight755@gmail.com>
2021-09-12 07:21:04 +0800

    src/info: Added more methods
    This replaces the need of user to do info::full(userID/username).results.attributes.....
    With this, the user can just simply do info::get_bl(userID/username) to get a bool value if the passed userID/username is blacklisted or not.
    
    Suggested-by: PeroSar <perosar1111@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- src/info/mod.rs
+++ src/info/mod.rs
@@ -14,3 +14,160 @@ pub async fn full<T: std::fmt::Display>(user: T) -> ApiResp {
         .unwrap();
     return res
 }
+
+pub fn get_flag<T: std::fmt::Display>(user: T) -> Option<String> {
+    return full(user).results.attributes.blacklist_flag
+}
+
+pub fn get_success<T: std::fmt::Display>(user: T) -> bool {
+    return full(user).success
+}
+
+pub fn get_resp<T: std::fmt::Display>(user: T) -> i16 {
+    return full(user).response_code
+}
+
+pub fn get_bl<T: std::fmt::Display>(user: T) -> bool {
+    return full(user).results.attributes.is_blacklisted
+}
+
+pub fn get_type<T: std::fmt::Display>(user: T) ->  String {
+    return full(user).results.entity_type
+}
+
+pub fn get_original_ptid<T: std::fmt::Display>(user: T) -> Option<String> {
+    return full(user).results.attributes.original_private_id
+}
+
+pub fn get_user_verified<T: std::fmt::Display>(user: T) -> bool {
+    return full(user).results.attributes.intellivoid_accounts_verified
+}
+
+pub fn get_user_operator<T: std::fmt::Display>(user: T) -> bool {
+    return full(user).results.attributes.is_operator
+}
+
+pub fn get_user_agent<T: std::fmt::Display>(user: T) -> bool {
+    return full(user).results.attributes.is_agent
+}
+
+pub fn get_user_whitelisted<T: std::fmt::Display>(user: T) -> bool {
+    return full(user).results.attributes.is_whitelisted
+}
+
+pub fn get_user_official<T: std::fmt::Display>(user: T) -> bool {
+    return full(user).results.attributes.is_official
+}
+
+pub fn get_reason<T: std::fmt::Display>(user: T) -> Option<String> {
+    return full(user).results.attributes.blacklist_reason
+}
+
+pub fn get_potential<T: std::fmt::Display>(user: T) -> bool {
+    return full(user).results.attributes.is_potential_spammer
+}
+
+pub fn get_flag_evade<T: std::fmt::Display>(user: T) -> bool {
+    if full(user).results.attributes.blacklist_flag.unwrap_or("None".to_string()) == "0xEVADE" {
+	return true;
+    } else {
+	return false;
+    }
+}
+
+pub fn get_flag_spam<T: std::fmt::Display>(user: T) -> bool {
+    if full(user).results.attributes.blacklist_flag.unwrap_or("None".to_string()) == "0xSPAM" {
+	return true;
+    } else {
+	return false;
+    }
+}
+
+pub fn get_flag_scam<T: std::fmt::Display>(user: T) -> bool {
+    if full(user).results.attributes.blacklist_flag.unwrap_or("None".to_string()) == "0xSCAM" {
+	return true;
+    } else {
+	return false;
+    }
+}
+
+
+pub fn get_flag_cacp<T: std::fmt::Display>(user: T) -> bool {
+    if full(user).results.attributes.blacklist_flag.unwrap_or("None".to_string()) == "0xCACP" {
+	return true;
+    } else {
+	return false;
+    }
+}
+
+pub fn get_flag_sp<T: std::fmt::Display>(user: T) -> bool {
+    if full(user).results.attributes.blacklist_flag.unwrap_or("None".to_string()) == "0xSP" {
+	return true;
+    } else {
+	return false;
+    }
+}
+
+pub fn get_flag_piracy<T: std::fmt::Display>(user: T) -> bool {
+    if full(user).results.attributes.blacklist_flag.unwrap_or("None".to_string()) == "0xPIRACY" {
+	return true;
+    } else {
+	return false;
+    }
+}
+
+pub fn get_flag_namespam<T: std::fmt::Display>(user: T) -> bool {
+    if full(user).results.attributes.blacklist_flag.unwrap_or("None".to_string()) == "0xNAMESPAM" {
+	return true;
+    } else {
+	return false;
+    }
+}
+
+pub fn get_flag_massadd<T: std::fmt::Display>(user: T) -> bool {
+    if full(user).results.attributes.blacklist_flag.unwrap_or("None".to_string()) == "0xMASSADD" {
+	return true;
+    } else {
+	return false;
+    }
+}
+
+pub fn get_flag_imper<T: std::fmt::Display>(user: T) -> bool {
+    if full(user).results.attributes.blacklist_flag.unwrap_or("None".to_string()) == "0xIMPER" {
+	return true;
+    } else {
+	return false;
+    }
+}
+
+pub fn get_flag_raid<T: std::fmt::Display>(user: T) -> bool {
+    if full(user).results.attributes.blacklist_flag.unwrap_or("None".to_string()) == "0xRAID" {
+	return true;
+    } else {
+	return false;
+    }
+}
+
+pub fn get_flag_private<T: std::fmt::Display>(user: T) -> bool {
+    if full(user).results.attributes.blacklist_flag.unwrap_or("None".to_string()) == "0xPRIVATE" {
+	return true;
+    } else {
+	return false;
+    }
+}
+
+pub fn get_spam_predict<T: std::fmt::Display>(user: T) ->  Option<f64> {
+    return full(user).results.spam_prediction.spam_prediction
+}
+
+pub fn get_ham_predict<T: std::fmt::Display>(user: T) -> Option<f64> {
+    return full(user).results.spam_prediction.ham_prediction
+}
+
+pub fn get_lang<T: std::fmt::Display>(user: T) ->  Option<String> {
+    return full(user).results.language_prediction.language
+}
+
+pub fn get_probability<T: std::fmt::Display>(user: T) -> Option<f64> {
+    return full(user).results.language_prediction.probability
+}
