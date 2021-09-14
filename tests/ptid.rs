7b8e91778685f9ace44eddfe9f3e77a8bbdb0066
PeroSar <perosar1111@gmail.com>
2021-09-14 12:39:05 +0530

    tests: update according to latest version
    Signed-off-by: PeroSar <perosar1111@gmail.com>

--- tests/ptid.rs
+++ tests/ptid.rs
@@ -3,7 +3,7 @@ use spamprotection::info;
 #[test]
 fn test_ptid() {
     assert_eq!(
-        info::get_ptid("@Pero_Sar1111"),
+        info::full("@Pero_Sar1111").get_ptid(),
         "TEL-9b7ef46a550112edea3ba46220283b1bfd8feddd09b26888524ef7245947e97f-b105a169"
     );
 }
